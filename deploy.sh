8c23a1b275f80c9f6eff8bac6ab09bf894cce93c
dinal.infinity <dinal.infinity@infinity.com>
2023-11-23 13:36:55 +0700

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-docker build . -t frontend
 docker stop ticket
 docker rm -f ticket
+docker build . -t fronten
 docker run --hostname ticket --name ticket --network frontendnet -d frontend
