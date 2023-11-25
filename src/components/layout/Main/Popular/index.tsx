5b71bc1689bb113b65e46d47c2cc1376e4b52d36
dinal.infinity <dinal.infinity@infinity.com>
2023-11-25 09:16:25 +0700

    updated popular

--- src/components/layout/Main/Popular/index.tsx
+++ src/components/layout/Main/Popular/index.tsx
@@ -25,7 +25,7 @@ const Popular = () => {
     >
       <div className="w-full">
         <h1 className="font-bold text-2xl pb-4">Popular this week</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-16 md:gap-8 sm:gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-16 md:gap-8 sm:gap-4">
           <CardEvent className="bg-[url('../../public/album_1.jpg')] bg-cover bg-center hover:opacity-70 shadow-lg ease-in-out transform hover:scale-105 transition duration-500" />
           <CardEvent className="bg-[url('../../public/album_2.webp')] bg-cover bg-center hover:opacity-70 shadow-lg transform hover:scale-105 transition duration-500" />
           <CardEvent className="bg-[url('../../public/album_3.jpg')] bg-cover bg-center hover:opacity-70 shadow-lg transform hover:scale-105 transition duration-500" />
