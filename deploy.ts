337d0fd7b3e261a9075af348c3f3a11abc4c30e5
dinal.infinity <dinal.infinity@infinity.com>
2023-11-27 11:55:50 +0700

    testing deploy

---
+++ deploy.ts
@@ -0,0 +1,19 @@
+import { exec } from 'child_process';
+
+exec('git pull', (error: Error | null, stdout: string, stderr: string) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
+
+exec('docker-compose up --build -d', (error: Error | null, stdout: string, stderr: string) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
