8bcbb0d357b707c62fb2b6f6e66bc7e10752e6c1
dinal.infinity <dinal.infinity@infinity.com>
2023-11-27 11:59:38 +0700

    updated

--- deploy.ts
+++ deploy.ts
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('git pull', (error: Error | null, stdout: string, stderr: string) => {
+exec('git pull', (error: any, stdout: string, stderr: string) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
@@ -9,7 +9,7 @@ exec('git pull', (error: Error | null, stdout: string, stderr: string) => {
   console.error(`stderr: ${stderr}`);
 });
 
-exec('docker-compose up --build -d', (error: Error | null, stdout: string, stderr: string) => {
+exec('docker-compose up --build -d', (error: any,  stdout: string, stderr: string) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
